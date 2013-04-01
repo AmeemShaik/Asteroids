703b54beb3bb238897cbaadca8be983ec682086e
Ameem Shaik <shaik@live.unc.edu>
2013-03-31 20:57:14 -0400

    GameGui

--- AsteroidGame/Assets/Asteroids/GameGUI.js
+++ AsteroidGame/Assets/Asteroids/GameGUI.js
@@ -13,6 +13,45 @@ function Start(){
 function OnGUI() {
 	GUI.contentColor = Color.white;
 	questionImage = Instantiate(Resources.Load("drug_label"));
+	var problem = getProblem() as XmlNode;
+	if(problem.HasChildNodes){
+		var question = problem.ChildNodes[0].InnerText;
+		var answer1 = problem.ChildNodes[1].InnerText;
+		var answer2 = problem.ChildNodes[2].InnerText;
+		var answer3 = problem.ChildNodes[3].InnerText;
+		var answer4 = problem.ChildNodes[4].InnerText;
+		var difficulty = problem.ChildNodes[5].InnerText;
+		var correctAnswer = problem.ChildNodes[6].InnerText;
+		var image = problem.ChildNodes[7].InnerText;
+ 	}
+	
+	GUILayout.BeginArea (Rect (0,0,Screen.width,Screen.height));
+		GUILayout.BeginHorizontal();
+			if(GUILayout.Button("Go Home", GUILayout.Height(buttonHeight),GUILayout.Width(buttonWidth))){
+				Application.LoadLevel("Title");
+			}
+			GUILayout.BeginVertical();
+				GUILayout.Label(question);
+				GUILayout.Label(answer1 + " " + answer2 + " " + answer3 + " " + answer4);
+			GUILayout.EndVertical();
+			if(GUILayout.Button(questionImage, GUILayout.Height(buttonHeight),GUILayout.Width(buttonWidth))){
+				if(popupImage){popupImage = false;}
+				else{popupImage = true;}
+			}
+		GUILayout.EndHorizontal();
+	GUILayout.EndArea();
+	
+	if(popupImage){
+		sWidth = Screen.width;
+		sHeight = Screen.height;
+		GUILayout.BeginArea (Rect (sWidth/3, sHeight/3, sWidth/2, sHeight/2));
+			GUILayout.Label (questionImage);
+		GUILayout.EndArea();
+	}
+	
+/*
+	GUI.contentColor = Color.white;
+	questionImage = Instantiate(Resources.Load("drug_label"));
 	miniPicSize = 65;
 	picSize = 400;
 	var problem = getProblem() as XmlNode;
@@ -42,6 +81,7 @@ function OnGUI() {
 	if(popupImage){
 		GUI.Label (Rect (Screen.width/2-125, Screen.height/2-125, picSize, picSize), questionImage);
 	}
+	*/
 }
 
 function getProblem(){
