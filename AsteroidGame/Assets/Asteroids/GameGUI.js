b820b56d28f0cc5b733de4fc16353d05fc24ed2c
Ameem Shaik <shaik@live.unc.edu>
2013-04-03 22:28:14 -0400

    Fixed the memory leak

--- AsteroidGame/Assets/Asteroids/GameGUI.js
+++ AsteroidGame/Assets/Asteroids/GameGUI.js
@@ -5,37 +5,33 @@ public var popupImage : boolean = false;
 public var currentQuestion;
 public var buttonHeight:int = 50;
 public var buttonWidth:int = 100;
+public var problem;
+public var sWidth = Screen.width;
+public var sHeight = Screen.height;
+public var image;
 function Awake(){
 	loadQuestions();
 	currentQuestion = 0;
-	var problem = getProblem() as XmlNode;
+	problem = getProblem() as XmlNode;
 	Camera.main.GetComponent.<JSExample3>().currentProblem = problem;
+	var image = problem.ChildNodes[7].InnerText;
+	image = image.Substring(0, image.length-4);
+	questionImage = Instantiate(Resources.Load(image));
 }
 
 function OnGUI() {
 	GUI.contentColor = Color.white;
-	var problem = getProblem() as XmlNode;
-	Camera.main.GetComponent.<JSExample3>().currentProblem = problem;
-	if(problem.HasChildNodes){
-		var question = problem.ChildNodes[0].InnerText;
-		var answer1 = problem.ChildNodes[1].InnerText;
-		var answer2 = problem.ChildNodes[2].InnerText;
-		var answer3 = problem.ChildNodes[3].InnerText;
-		var answer4 = problem.ChildNodes[4].InnerText;
-		var difficulty = problem.ChildNodes[5].InnerText;
-		var correctAnswer = problem.ChildNodes[6].InnerText;
-		var image = problem.ChildNodes[7].InnerText;
- 	}
- 	image = image.Substring(0, image.length-4);
-	questionImage = Instantiate(Resources.Load(image));
 	GUILayout.BeginArea (Rect (0,0,Screen.width,Screen.height));
 		GUILayout.BeginHorizontal();
 			if(GUILayout.Button("Go Home", GUILayout.Height(buttonHeight),GUILayout.Width(buttonWidth))){
 				Application.LoadLevel("Title");
 			}
 			GUILayout.BeginVertical();
-				GUILayout.Label(question);
-				GUILayout.Label(answer1 + " " + answer2 + " " + answer3 + " " + answer4);
+				GUILayout.Label(problem.ChildNodes[0].InnerText);
+				GUILayout.Label(problem.ChildNodes[1].InnerText + 
+				" " + problem.ChildNodes[2].InnerText + 
+				" " + problem.ChildNodes[3].InnerText + 
+				" " + problem.ChildNodes[4].InnerText);
 			GUILayout.EndVertical();
 			if(GUILayout.Button(questionImage, GUILayout.Height(buttonHeight),GUILayout.Width(buttonWidth))){
 				if(popupImage){popupImage = false;}
@@ -45,46 +41,10 @@ function OnGUI() {
 	GUILayout.EndArea();
 	
 	if(popupImage){
-		sWidth = Screen.width;
-		sHeight = Screen.height;
 		GUILayout.BeginArea (Rect (sWidth/3, sHeight/3, sWidth/2, sHeight/2));
-			GUILayout.Label (questionImage);
+		GUILayout.Label (questionImage);
 		GUILayout.EndArea();
 	}
-	
-/*
-	GUI.contentColor = Color.white;
-	questionImage = Instantiate(Resources.Load("drug_label"));
-	miniPicSize = 65;
-	picSize = 400;
-	var problem = getProblem() as XmlNode;
-	if(problem.HasChildNodes){
-		var question = problem.ChildNodes[0].InnerText;
-		var answer1 = problem.ChildNodes[1].InnerText;
-		var answer2 = problem.ChildNodes[2].InnerText;
-		var answer3 = problem.ChildNodes[3].InnerText;
-		var answer4 = problem.ChildNodes[4].InnerText;
-		var difficulty = problem.ChildNodes[5].InnerText;
-		var correctAnswer = problem.ChildNodes[6].InnerText;
-		var image = problem.ChildNodes[7].InnerText;
- 	}
-
-	if(GUILayout.Button("Go Home", GUILayout.Height(buttonHeight),GUILayout.Width(buttonWidth))){
-				Application.LoadLevel("Title");
-			}
-	GUI.Label (Rect (Screen.width/2 - 150, 10, 300, 25), question);
-	GUI.Label (Rect (Screen.width/2- 75, 30, 150, 30), answer1 + " " + answer2 + " " + answer3 + " " + answer4);
-	if(GUI.Button (Rect (Screen.width - miniPicSize-5,5, miniPicSize, miniPicSize), questionImage)){
-		if(popupImage){popupImage = false;}
-		else{popupImage = true;}
-	}
-	GUI.Label (Rect (0, miniPicSize-5, Screen.width+10, 20), "__________________________________________________________________________________________________________________________________________________________________________________________________");
-
-	//Toggles image popup
-	if(popupImage){
-		GUI.Label (Rect (Screen.width/2-125, Screen.height/2-125, picSize, picSize), questionImage);
-	}
-	*/
 }
 
 function getProblem(){
@@ -99,6 +59,9 @@ function loadQuestions(){
 }
 function nextQuestion(){
 	currentQuestion++;
-	var problem = getProblem() as XmlNode;
+	problem = getProblem() as XmlNode;
 	Camera.main.GetComponent.<JSExample3>().currentProblem = problem;
+	var image = problem.ChildNodes[7].InnerText;
+	image = image.Substring(0, image.length-4);
+	questionImage = Instantiate(Resources.Load(image));
 }
